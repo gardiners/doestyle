b82c0753ec16eeaf2a009bbf091086d8a93db4f4
Sam Gardiner <7697866+gardiners@users.noreply.github.com>
2026-01-22 16:05:14 +1100

    Rewrite `check_font_families()` to check for presence of Public Sans TTF files
    Resolves #31

--- R/load_showtext.R
+++ R/load_showtext.R
@@ -1,26 +1,46 @@
-#' Import showtext to make use of DoE fonts
+#' Activate `showtext` and check for presence of Public Sans font family
 #'
+#' [theme_doe()] uses `showtext` to allow R graphics devices to render the
+#' Public Sans font family. Use `check_font_families()` to check that Public
+#' Sans is available and to activate `showtext`.
+#'
+#' @return `check_font_families()` does not have a return value and is called
+#'   for its side effects.
+#'
+#' @seealso [showtext::showtext_auto()] and [Public
+#'   Sans](https://digitalnsw.github.io/public-sans/).
 #' @export
 check_font_families <- function() {
-  if (!suppressWarnings(any(file.exists(sysfonts::font_paths("Public Sans"))))) {
-    glue::glue_col("The font {underline Public Sans} is not installed in any of the search paths.
-  Please download the fonts from {blue https://fonts.google.com/specimen/Public+Sans}
-  And then install the fonts, following the instructions here:
-  {blue https://support.microsoft.com/en-us/windows/how-to-install-or-remove-a-font-in-windows-f12d0657-2fc8-7613-c76f-88d043b334b8#ID0EDH}
-  \n\n
-  ", .literal = TRUE)
-  }
+  if ("Public Sans" %in% sysfonts::font_families()) {
+    # Ensure any newly opened graphics device uses `showtext` to draw text
+    showtext::showtext_auto()
+  } else {
+    # Do the required files and faces exist?
+    fonts <- sysfonts::font_files()
+    public_sans <- fonts[fonts$family == "Public Sans",]
+    faces <- list(regular = "PublicSans-Regular.ttf",
+                  bold = "PublicSans-Bold.ttf",
+                  italic = "PublicSans-Italic.ttf",
+                  bolditalic = "PublicSans-BoldItalic.ttf")
 
-  if ((.Platform$OS.type == "windows") & (!"Public Sans" %in% sysfonts::font_families())) {
-    sysfonts::font_add("Public Sans",
-      regular = "PublicSans-Regular.ttf",
-      bold = "PublicSans-Bold.ttf",
-      italic = "PublicSans-Italic.ttf",
-      bolditalic = "PublicSans-BoldItalic.ttf"
-    )
-    # font.families()
+    if (setequal(intersect(public_sans$file, faces), faces)) {
+      sysfonts::font_add("Public Sans",
+                         regular = faces$regular,
+                         bold = faces$bold,
+                         italic = faces$italic,
+                         bolditalic = faces$bolditalic)
+    } else {
+       cli::cli_warn(
+        message = c(
+          "!" = "The font family {.strong Public Sans} is not installed in any of
+              your operating system's font search paths. {.fn theme_doe}
+              requires `PublicSans-Regular.ttf`, `PublicSans-Bold.ttf`,
+              `PublicSans-Italic.ttf`, and `PublicSans-BoldItalic.ttf`.",
+          "i" = "Please download and install Public Sans from
+              {.url https://digitalnsw.github.io/public-sans/download/}."
+        )
+      )
+    }
+    showtext::showtext.auto()
   }
-
-  # set showtext_auto()  ----------------------------------------------------
-  showtext::showtext_auto() # call once so any newly opened graphics device uses showtext to draw text
 }
